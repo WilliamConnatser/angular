c57a955c83edf54a484b3ca842f6801fe0e28391
William Connatser <43946230+WilliamConnatser@users.noreply.github.com>
2023-05-08 21:53:58 -0400

    created todos component via CLI and created my todo list for this app

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Todos } from '../common/models/todos';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss'],
+})
+export class TodosComponent {
+  // Initial app logic:
+  // 1. Render todos list
+  // 2. Select a todo
+  // 3. Render selected todo
+
+  todos: Todos[] = [
+    {
+      title: 'Finish initial app logic',
+      description: 'Finish yet another todo app, but angular style!',
+      progress: 0.1,
+      complete: false,
+    },
+    {
+      title: 'Add random Angular features',
+      description:
+        'Add random features to use more advanced Angular primitives',
+      progress: 0,
+      complete: false,
+    },
+    {
+      title: 'Add random NgRx and RxJs features',
+      description:
+        'Add random features that use NgRx and RxJs more extensively',
+      progress: 0,
+      complete: false,
+    },
+    {
+      title: 'Write a bunch of tests',
+      description:
+        'Write unit tests and e2e tests which achieve 90% test coverage',
+      progress: 0,
+      complete: false,
+    },
+  ];
+}
